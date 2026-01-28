fe5d3b10c6f04ef82db5f40e9d165c743e5d4b02
Ammar Khan <57397520+ammarkhanone@users.noreply.github.com>
2026-01-28 23:50:06 +0500

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>live-demo-app022</h1>
+      <p>create a simle restjurent website using typescript and next js </p>
+    </main>
+  )
+}
